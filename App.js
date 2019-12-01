4b303869c7042d77829e3af24700653977d71c88
shikhark <conveytoshikhar@gmail.com>
2019-12-01 15:38:05 +0800

    Added custom fonts, ready to proceed

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LandingScreen from './screens/LandingScreen'
+import * as Font from 'expo-font';
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false)
+  const[isAppReady, setIsAppReady] = useState(false)
+
+  async function setup(){
+    await Font.loadAsync({
+      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+      'open-sans-light': require('./assets/fonts/OpenSans-Light.ttf'),
+      'open-sans-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'open-sans-italic': require('./assets/fonts/OpenSans-Italic.ttf'),
+    });
+    setFontLoaded(true)
+    setIsAppReady(true)
+  }
+
+  // another variation of component did mount since there are no dependecies 
+  useEffect(() => {
+    setup()
+  },[])
+
   return (
     <View style={styles.container}>
-      <LandingScreen />
+      {isAppReady ? <LandingScreen /> : null}
     </View>
   );
 }
--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Text, View} from 'react-native'
+import { Text, View, StyleSheet} from 'react-native'
 
-const LandingScreen = (props) => {
-    return <Text>This is working from Landing Screen</Text>
+const LandingScreen = (props) => {    
+    return <Text style={styles.textStyle}>This is working from Landing Screen</Text>
 }
+
+const styles = StyleSheet.create({
+    textStyle : {
+        fontFamily: 'open-sans-italic'
+    }
+})
 
 export default LandingScreen
 
