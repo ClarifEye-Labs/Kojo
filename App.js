9bd75d2400157135861b9e2e1824122f8ce63c18
shikhark <conveytoshikhar@gmail.com>
2019-12-01 14:40:36 +0800

    Added Dummy Landing Screen Structure, Incorporated it in App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LandingScreen from './screens/LandingScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testing functionality!</Text>
+      <LandingScreen />
     </View>
   );
 }
---
+++ screens/LandingScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Text, View} from 'react-native'
+
+const LandingScreen = (props) => {
+    return <Text>This is working from Landing Screen</Text>
+}
+
+export default LandingScreen
+
