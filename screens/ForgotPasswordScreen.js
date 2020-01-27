50dbc8fece65f792907787f5371cd5aa28be97c1
shikhark <conveytoshikhar@gmail.com>
2020-01-27 17:36:14 +0800

    Forgot password screen prod ready

--- screens/ForgotPasswordScreen.js
+++ screens/ForgotPasswordScreen.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, TextInput} from 'react-native'
+import { View, StyleSheet, Text, TextInput } from 'react-native'
 import { Heading, Back, Icon, Button } from '../Components'
 import { dimens, colors, strings, customFonts } from '../constants'
-import { commonStyling } from '../common' 
+import { commonStyling } from '../common'
 import iconNames from '../constants/iconNames';
-import {PropTypes} from 'prop-types'
+import { PropTypes } from 'prop-types'
 import LottieView from 'lottie-react-native'
+import screens from '../constants/screens';
 
 class ForgotPasswordScreen extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      name: 'ForgotPassword',
-      emailEntered: ''
+      emailEntered: '',
+      emailErrorReason: undefined,
+      emailErrorStatus: false,
+      buttonLoading: false
     }
   }
 
@@ -24,7 +27,48 @@ class ForgotPasswordScreen extends Component {
   }
 
   submitButtonPressed = () => {
-    this.animation.play()
+    this.setState({
+      buttonLoading: true
+    }, () => this.checkForEmailError())
+  }
+
+  checkForEmailError = () => {
+    const { emailEntered } = this.state
+    let emailError = {
+      errorReason: undefined,
+      errorStatus: false
+    }
+    if (emailEntered.length === 0) {
+      emailError.errorReason = strings.emailCannotBeEmpty,
+        emailError.errorStatus = true
+    } else if (! /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(emailEntered)) {
+      emailError.errorStatus = true
+      emailError.errorReason = strings.emailErrorMessage
+    }
+    
+    if(emailError.errorStatus) {
+      this.setState({
+        emailErrorReason: emailError.errorReason,
+        emailErrorStatus: true,
+        buttonLoading: false
+      })
+    }else{
+      this.setState({
+        emailErrorReason: emailError.errorReason,
+        emailErrorStatus: false,
+      }, this.uploadInfoToDatabase)
+    }
+  }
+
+  uploadInfoToDatabase = () => {
+    console.log('Entering firebase')
+    this.setState({
+      buttonLoading: false
+    }, () => {this.animation.play()})
+  }
+
+  changeScreens = (navigation) => {
+    navigation.navigate(screens.EmailScreen)
   }
 
   render() {
@@ -35,43 +79,54 @@ class ForgotPasswordScreen extends Component {
       subHeadingStyle,
       textInputContainer,
       textInputStyle,
+      textInputStyleError,
       crossStyle,
       buttonStyle,
+      errorSubHeadingStyle,
       animationContainerStyle
     } = styles
 
     const {
       navigation
     } = this.props
-    
+
     return (
       <View style={mainContainer}>
-        <Icon nameIOS={iconNames.backIOS} nameAndroid={iconNames.backAndroid} style={commonStyling.backButtonStyling} size={34} />
-        <Icon nameIOS={iconNames.crossIOS} nameAndroid={iconNames.crossAndroid} style={crossStyle} size={50}/>
+        <Icon nameIOS={iconNames.backIOS} nameAndroid={iconNames.backAndroid} style={commonStyling.backButtonStyling} size={34} onPress={()=> navigation.goBack() } />
+        <Icon nameIOS={iconNames.crossIOS} nameAndroid={iconNames.crossAndroid} style={crossStyle} size={50} onPress={()=> navigation.goBack() } />
         <Heading containerStyle={headingContainerStyle} title={strings.forgotPassword} />
 
         <View style={inputContainer}>
           <Text style={subHeadingStyle}>{strings.enterYourEmailForgotPassword}</Text>
+          {this.state.emailErrorStatus
+            ? <Text style={errorSubHeadingStyle}>{this.state.emailErrorReason} </Text>
+            : null}
           <View style={textInputContainer}>
-          <TextInput 
-            style={textInputStyle}
-            onChange={this.setEmailEntered}
-            placeholder={strings.emailPlaceholderText} 
-            autoCapitalize='none'
-            autoCompleteType='email'
-            numberOfLines={1}
+            <TextInput
+              style={this.state.emailErrorStatus ? textInputStyleError : textInputStyle}
+              onChangeText={this.setEmailEntered}
+              placeholder={strings.emailPlaceholderText}
+              autoCapitalize='none'
+              autoCompleteType='email'
+              numberOfLines={1}
             />
           </View>
         </View>
         <View style={animationContainerStyle}>
-          <LottieView  
+          <LottieView
             ref={animation => {
               this.animation = animation;
             }}
+            onAnimationFinish={()=>this.changeScreens(navigation)}
             loop={false}
             source={require('../assets/animations/email_send.json')} />
         </View>
-        <Button title={strings.sendEmail} onPress={this.submitButtonPressed} style={buttonStyle} textColor={colors.colorAccent} />
+        <Button 
+          title={strings.sendEmail} 
+          isLoading={this.state.buttonLoading}
+          onPress={this.submitButtonPressed} 
+          style={buttonStyle} 
+          textColor={colors.colorAccent} />
       </View>
     );
   }
@@ -81,8 +136,13 @@ const styles = StyleSheet.create({
   mainContainer: {
     ...commonStyling.mainContainer,
   },
+  errorSubHeadingStyle: {
+    fontSize: 16,
+    fontFamily: customFonts.regular,
+    marginTop: 16,
+    color: colors.errorRed
+  },
   headingContainerStyle: {
-    // position: 'absolute',
     marginTop: 120,
     marginLeft: dimens.screenHorizontalMargin,
     marginRight: dimens.screenHorizontalMargin
@@ -102,14 +162,14 @@ const styles = StyleSheet.create({
     fontFamily: customFonts.regular
   },
   textInputContainer: {
-    borderBottomWidth: dimens.inputTextBorderWidth,
-    borderBottomColor: colors.blackTransluscent,
-    marginTop: 12,
+    marginTop: 8,
   },
   textInputStyle: {
     fontSize: dimens.inputTextFontSize + 6,
     color: colors.colorPrimary,
     fontFamily: customFonts.regular,
+    borderBottomWidth: dimens.inputTextBorderWidth,
+    borderBottomColor: colors.blackTransluscent,
     paddingVertical: 12,
   },
   buttonStyle: {
@@ -125,8 +185,16 @@ const styles = StyleSheet.create({
     height: 250,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  textInputStyleError: {
+    fontSize: dimens.inputTextFontSize + 6,
+    color: colors.colorPrimary,
+    fontFamily: customFonts.regular,
+    borderBottomWidth: dimens.inputTextBorderWidth,
+    borderBottomColor: colors.errorRed,
+    paddingVertical: 12,
   }
-  
+
 })
 
 ForgotPasswordScreen.navigationOptions = {
